d95bd668590fdc6014b8da44338a1f8e8b8cbbd0
ExPLICIT-27 <nandumahesh2004@gmail.com>
2024-09-30 14:50:50 +0530

    corrected code

--- maxSubTreeSum.c
+++ maxSubTreeSum.c
@@ -5,48 +5,57 @@ typedef struct tree{
     struct tree *left, *right;
 }tree;
 int max(int, int);
-int MaxSubtreeSum(tree *);
-int maxsumutil(tree *, int);
-tree *levelorderbuild(tree*, int*, int, int);
 tree *createnode(int);
+tree *buildTree(int*, int);
+tree *buildTreeHelper(int*, int*, int);
+int maxSum(tree*);
+int maxsumutil(tree*, int*);
 int main(){
-    int level[] = {1, 2, 3, 4, 5, 6, 7};
-    int level2[] = {1, -2, 3, 4, 5, -6, 2};
-    tree *root = levelorderbuild(root, level, 0, 7);
-    tree *root1 = levelorderbuild(root1, level2, 0, 7);
-    printf("%d\n", MaxSubtreeSum(root));
-    printf("%d", MaxSubtreeSum(root1));
+    int pre[] = {10, 2, 20, 1, 10, -25, 3, 4};
+    tree *root = buildTree(pre, 8);
+    printf("Max Sum: %d", maxSum(root));
     return 0;
+}
+int max(int a, int b){
+    return (a > b)? a : b;  
+}
+int maxsumutil(tree *root, int *ans){
+    if(root == NULL){
+        return 0;
+    }
+    int left = maxsumutil(root->left, ans);
+    int right = maxsumutil(root->right, ans);
+    int max_single = max(max(left, right) + root->data, root->data);
+    int max_top = max(max_single, left+right+root->data);
+    *ans = max(*ans, max_top);
+    return max_single;
+}
+int maxSum(tree *root){
+    if(root == NULL){
+        return 0;
+    }
+    int ans = INT_MIN;
+    maxsumutil(root, &ans);
+    return ans;
+}
+tree *buildTreeHelper(int *pre, int *idx, int size){
+    if(*idx >= size || pre[*idx] == -1){
+        (*idx)++;
+        return NULL;
+    }
+    tree *root = createnode(pre[*idx]);
+    (*idx)++;
+    root->left = buildTreeHelper(pre, idx, size);
+    root->right = buildTreeHelper(pre, idx, size);
+    return root;
+}
+tree *buildTree(int *pre, int size){
+    int idx = 0;
+    return buildTreeHelper(pre, &idx, size);
 }
 tree *createnode(int val){
     tree *newnode = (tree*)malloc(sizeof(tree));
     newnode->data = val;
     newnode->left = newnode->right = NULL;
     return newnode;
-}
-tree *levelorderbuild(tree *root, int *level, int i, int size){
-    if(i < size){
-        root = createnode(level[i]);
-        root->left = levelorderbuild(root, level, 2*i+1, size);
-        root->right = levelorderbuild(root, level, 2*i+2, size);
-        return root;
-    }
-    return NULL;
-}
-int MaxSubtreeSum(tree *root){
-    if(root == NULL){
-        return 0;
-    }
-    int ans = INT_MIN;
-    return maxsumutil(root, ans);
-}
-int maxsumutil(tree *root, int ans){
-    if(root == NULL){
-        return 0;
-    }
-    int currsum = root->data + maxsumutil(root->left, ans) + maxsumutil(root->right, ans);
-    return max(currsum, ans);
-}
-int max(int a, int b){
-    return (a > b)? a : b;
 }
