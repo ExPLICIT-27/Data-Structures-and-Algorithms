4b83f656b7ff19e4a2bcdc9d4b733b7ba3aa9e02
ExPLICIT-27 <nandumahesh2004@gmail.com>
2025-02-27 20:40:29 +0530

    LIS, LCM

--- LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence.cpp
@@ -45,6 +45,8 @@ int Memoized(string &str1, string &str2, int idx1, int idx2, vector<vector<int>>
 /*
  Tabulated bottom up Dynamic programming approach, 
  instead of recursive memo, iterative bottom up dp table
+ Along with the calculation of the length of the longest common substring,
+ printing it as well.
 */
 int TabulatedDp(string &str1, string &str2){
     int m = str1.length(), n = str2.length();
@@ -63,11 +65,43 @@ int TabulatedDp(string &str1, string &str2){
     }
     return dp[m][n];
 }
+string PrintLCS(string &str1, string &str2){
+    int n = str1.length(), m = str2.length();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(str1[i-1] == str2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    int i = n, j = m;
+    string res = "";
+    while(i > 0 && j > 0){
+        if(str1[i-1] == str2[j-1]){
+            res += str1[i-1];
+            i--, j--;
+        }
+        else if(dp[i-1][j] > dp[i][j-1]){
+            i--;
+        }
+        else{
+            j--;
+        }
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
 int main(){
     string str1 = "abatd", str2 = "bacd";
     cout << NaiveBruteForce(str1, str2, str1.length()-1, str2.length()-1) << endl;
     vector<vector<int>> memo(str1.length(), vector<int>(str2.length(), -1));
     cout << Memoized(str1, str2, str1.length()-1, str2.length()-1, memo) << endl;
-    cout << TabulatedDp(str1, str2);
+    cout << TabulatedDp(str1, str2) << endl;
+    cout << PrintLCS(str1, str2);
     return 0;
 }
