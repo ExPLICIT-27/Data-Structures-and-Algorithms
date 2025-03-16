4a2350f8c46f6047b2c0e3148c0a776e3885546d
ExPLICIT-27 <nandumahesh2004@gmail.com>
2025-03-17 00:31:50 +0530

    corrected LPS

--- LongestPalindrome.cpp
+++ LongestPalindrome.cpp
@@ -1,130 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 /*
-Given 2 strings, return the length of the longest common subsequence,
-Eg str1 = "abatd"
-str2 = "bacd"
-result = 3 ("bad")
-
+find the longest palindromic subsequence
 */
-/*
-    Naive Brute Force (O(2^(m*n))) (top down)
-    Pass the strings and their lengths,
-    check if str[idx1] == str2[idx2]
-    if so, return 1 + func(str1, str2, idx1-1, idx2-1)
-    else, return max(func(str1, str2, idx1, idx2-1), func(str1, str2, idx-1, idx2))
-*/
-
-int NaiveBruteForce(string &str1, string &str2, int idx1, int idx2)
+string NaiveBrute(int left, int right, string &str)
 {
-    if (idx1 < 0 || idx2 < 0)
-        return 0;
-
-    if (str1[idx1] == str2[idx2])
-    {
-        return 1 + NaiveBruteForce(str1, str2, idx1 - 1, idx2 - 1);
-    }
-    return max(NaiveBruteForce(str1, str2, idx1 - 1, idx2), NaiveBruteForce(str1, str2, idx1, idx2 - 1));
-}
-/*
-memoized approach, use a memo table m*n
-*/
-int Memoized(string &str1, string &str2, int idx1, int idx2, vector<vector<int>> &memo)
-{
-    if (idx1 < 0 || idx2 < 0)
-        return 0;
-    if (memo[idx1][idx2] != -1)
-        return memo[idx1][idx2];
-
-    int res;
-    if (str1[idx1] == str2[idx2])
-    {
-        res = 1 + Memoized(str1, str2, idx1 - 1, idx2 - 1, memo);
-    }
-    else
-    {
-        res = max(Memoized(str1, str2, idx1, idx2 - 1, memo), Memoized(str1, str2, idx1 - 1, idx2, memo));
-    }
-    memo[idx1][idx2] = res;
-    return res;
-}
-/*
- Tabulated bottom up Dynamic programming approach,
- instead of recursive memo, iterative bottom up dp table
- Along with the calculation of the length of the longest common substring,
- printing it as well.
-*/
-int TabulatedDp(string &str1, string &str2)
-{
-    int m = str1.length(), n = str2.length();
-
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-    for (int i = 1; i < m + 1; i++)
-    {
-        for (int j = 1; j < n + 1; j++)
-        {
-            if (str1[i - 1] == str2[j - 1])
-            {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            }
-            else
-            {
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-            }
-        }
-    }
-    return dp[m][n];
-}
-string PrintLCS(string &str1, string &str2)
-{
-    int n = str1.length(), m = str2.length();
-
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            if (str1[i - 1] == str2[j - 1])
-            {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            }
-            else
-            {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-    int i = n, j = m;
     string res = "";
-    while (i > 0 && j > 0)
+    if (left == right)
     {
-        if (str1[i - 1] == str2[j - 1])
-        {
-            res += str1[i - 1];
-            i--, j--;
-        }
-        else if (dp[i - 1][j] > dp[i][j - 1])
-        {
-            i--;
-        }
-        else
-        {
-            j--;
-        }
+        res += str[left];
+        return res;
     }
-    reverse(res.begin(), res.end());
-    return res;
+    if (left > right)
+        return res;
+
+    if (str[left] == str[right])
+    {
+        return str[left] + NaiveBrute(left + 1, right - 1, str) + str[right];
+    }
+    string incl = NaiveBrute(left + 1, right, str);
+    string incr = NaiveBrute(left, right - 1, str);
+    return (incl.length() > incr.length()) ? incl : incr;
+}
+string Memoized(int left, int right, string &str, map<string, string> &memo)
+{
+    string res = "";
+    if (left == right)
+    {
+        return res + str[left];
+    }
+    if (left > right)
+    {
+        return res;
+    }
+    string key = to_string(left) + "," + to_string(right);
+    if (memo.find(key) != memo.end())
+    {
+        return memo[key];
+    }
+    if (str[left] == str[right])
+    {
+        return str[left] + Memoized(left + 1, right - 1, str, memo) + str[right];
+    }
+    string front = Memoized(left + 1, right, str, memo);
+    string back = Memoized(left, right - 1, str, memo);
+    memo[key] = (front.length() > back.length()) ? front : back;
+    return memo[key];
 }
 int main()
 {
-    string str1 = "abatd", str2 = "bacd";
-    cout << NaiveBruteForce(str1, str2, str1.length() - 1, str2.length() - 1) << endl;
-    vector<vector<int>> memo(str1.length(), vector<int>(str2.length(), -1));
-    cout << Memoized(str1, str2, str1.length() - 1, str2.length() - 1, memo) << endl;
-    cout << TabulatedDp(str1, str2) << endl;
-    cout << PrintLCS(str1, str2);
+    string str = "abcdfba";
+    cout << NaiveBrute(0, str.length() - 1, str) << endl;
+    map<string, string> memo;
+    cout << Memoized(0, str.length() - 1, str, memo) << endl;
     return 0;
 }
