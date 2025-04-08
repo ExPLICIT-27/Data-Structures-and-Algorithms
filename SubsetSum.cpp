a28886ade6bb87e7cc9f766febcdb201cfa1fd6a
ExPLICIT-27 <nandumahesh2004@gmail.com>
2025-04-08 09:40:31 +0530

    enhanced subset sum

--- SubsetSum.cpp
+++ SubsetSum.cpp
@@ -5,38 +5,38 @@ gives the sum k
 #include <bits/stdc++.h>
 using namespace std;
 
-bool subsetSum(vector<int> &numset, int index, int k, vector<int> &subset)
+void subsetSum(vector<vector<int>> &ans, vector<int> &nums, int index, vector<int> &current, int target)
 {
-    if (k == 0)
-        return true;
-    if (index >= numset.size())
-        return false;
-    for (int i = index; i < numset.size(); i++)
+    if (target == 0)
     {
-        if (k - numset[i] >= 0)
-        {
-            subset.push_back(numset[i]);
-            if (subsetSum(numset, i + 1, k - numset[i], subset))
-                return true;
-            subset.pop_back();
-        }
+        ans.push_back(current);
+        return;
     }
-    return false;
+    if (index >= nums.size())
+        return;
+    for (int i = index; i < nums.size(); i++)
+    {
+        if (nums[i] > target)
+            continue;
+        current.push_back(nums[i]);
+        subsetSum(ans, nums, i + 1, current, target - nums[i]);
+        current.pop_back();
+    }
 }
 int main()
 {
-    vector<int> numset{1, 2, 3};
-    int k = 4;
-    vector<int> subset;
-    if (subsetSum(numset, 0, k, subset))
+    vector<int> numset{2, 2, 4, 3, 5, 7};
+    int k = 7;
+    vector<vector<int>> ans;
+    vector<int> temp;
+    subsetSum(ans, numset, 0, temp, k);
+    for (auto arr : ans)
     {
-        cout << "Subset : ";
-        for (int num : subset)
-            cout << num << " ";
-    }
-    else
-    {
-        cout << "Not possible";
+        for (int i : arr)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
