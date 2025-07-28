a8c943633ef17b8470efdd813613c06c3d83f91b
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-07-28 15:14:51 +0530

    Min heap version of djikstras

--- Djikstras.cpp
+++ Djikstras.cpp
@@ -7,61 +7,54 @@ class Graph
 {
 private:
     int V;
-    vector<vector<int>> adjMatrix;
-    int minDist(vector<int> &dist, vector<bool> &visited)
+    vector<vector<pair<int, int>>> adjList; // node and weight
+    
+
+public:
+    Graph(int v) : V(v)
     {
-        int mindist = INT_MAX, minidx = -1;
-        for (int i = 0; i < V; i++)
-        {
-            if (!visited[i] && dist[i] < mindist)
-            {
-                mindist = dist[i], minidx = i;
-            }
-        }
-        return minidx;
+        adjList.resize(V);
     }
-    void printPath(vector<int> &parents, int dest)
+    void addEdge(int u, int v, int w)
     {
-        if (parents[dest] == -1)
-        {
-            cout << dest;
+        adjList[u].push_back({v, w});
+        adjList[v].push_back({u, w});
+    }
+    void printPath(vector<int> parents, int dest){
+        if(parents[dest] == -1){
+            cout  << dest;
             return;
         }
         printPath(parents, parents[dest]);
         cout << " -> " << dest;
     }
-
-public:
-    Graph(int v) : V(v)
-    {
-        adjMatrix.resize(V, vector<int>(V, 0));
-    }
-    void addEdge(int u, int v, int w)
-    {
-        adjMatrix[u][v] = adjMatrix[v][u] = w;
-    }
     void DjikstrasAlgo(int src, int dest)
     {
         vector<int> dist(V, INT_MAX);
-        vector<bool> visited(V, false);
         vector<int> parents(V, -1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // weight first, for the priority queue comparator
         dist[src] = 0;
-        for (int i = 0; i < V - 1; i++)
-        {
-            int u = minDist(dist, visited);
-            visited[u] = true;
-            for (int v = 0; v < V; v++)
-            {
-                if (adjMatrix[u][v] && !visited[v] && (adjMatrix[u][v] + dist[u] < dist[v]))
-                {
-                    dist[v] = adjMatrix[u][v] + dist[u];
+        pq.push({0, src});
+
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int u = pq.top().second;
+            pq.pop();
+
+            if(d > dist[u]) continue;
+
+            for(auto &[v, weight] : adjList[u]){
+                if(dist[u] + weight < dist[v]){
+                    dist[v] = dist[u] + weight;
                     parents[v] = u;
+                    pq.push({dist[v], v});
                 }
             }
         }
         cout << "\nDistance from " << src << " to " << dest << " : " << dist[dest];
         cout << "\nPath : ";
         printPath(parents, dest);
+        cout << "\n";
     }
 };
 int main()
