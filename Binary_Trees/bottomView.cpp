4bd90020726ac4d7a6262d786f5a775d8ca7642a
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-09-02 16:43:15 +0530

    Bottom view of binary tree

---
+++ Binary_Trees/bottomView.cpp
@@ -0,0 +1,44 @@
+/*
+Given a Binary Tree, find the bottom view of the tree
+        1
+    2       3
+4       5       6
+bottom view = 4, 5, 6
+using the same concept as vertical traversal, except we store only one node per
+column
+just remove the condition for checking map from topview, you have bottom view
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val){
+        this->val = val;
+        left = right = NULL;
+    }
+};
+vector<int> bottomTreeView(TreeNode *root){
+    if(!root)
+        return;
+    map<int, int> mp;
+    queue<pair<TreeNode *, int>> q;
+    q.push({root, 0});
+    while(!q.empty()){
+        auto curr = q.front();
+        q.pop();
+        mp[curr.second] = curr.first->val;
+        if(curr.first->left)
+            q.push({curr.first->left, curr.second - 1});
+        if(curr.first->right)
+            q.push({curr.first->right, curr.second + 1});
+    }
+    vector<int> result;
+    for(auto pair : mp)
+        result.push_back(pair.second);
+    return result;
+}
+int main(){
+    return 0;
+}
---
+++ Binary_Trees/topView.cpp
@@ -0,0 +1,44 @@
+/*
+Given a Binary Tree, find the top view of the tree
+        1
+    2       3
+4       5       6
+top view = 4, 2, 1, 3, 6
+using the same concept as vertical traversal, except we store only one node per
+column
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val){
+        this->val = val;
+        left = right = NULL;
+    }
+};
+vector<int> TopTreeView(TreeNode *root){
+    if(!root)
+        return {};
+    map<int, int> mp;
+    queue<pair<TreeNode *, int>> q;
+    q.push({root, 0});
+    while(!q.empty()){
+        auto curr_node = q.front();
+        q.pop();
+        if(!mp.count(curr_node.second))
+            mp[curr_node.second] = curr_node.first->val;
+        if(curr_node.first->left)
+            q.push({curr_node.first->left, curr_node.second - 1});
+        if(curr_node.first->right)
+            q.push({curr_node.first->right, curr_node.second + 1});
+    }
+    vector<int> result;
+    for(auto pair : mp)
+        result.push_back(pair.second);
+    return result;
+}
+int main(){
+    return 0;
+}
