5d6aeb7ed30af22edebb6d6f7658c9425dec72b1
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-09-06 10:26:40 +0530

    GFG find all connected components of an undirected graph

---
+++ GRAPHS/GFG_Connected_components_in_undirectedGraph.cpp
@@ -0,0 +1,37 @@
+/*
+given an undirected graph with 0 - V-1 vertices, find all the connected components
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(unordered_map<int, vector<int>> &G, int u, vector<bool> &visited, vector<int> &connected){
+    visited[u] = true;
+    connected.push_back(u);
+
+    for(int v : G[u]){
+        if(!visited[v])
+            dfs(G, v, visited, connected);
+    }
+}
+vector<vector<int>> findConnected(vector<vector<int>> &edges, int V){
+    unordered_map<int, vector<int>> G;
+
+    for(auto edge : edges){
+        G[edge[0]].push_back(edge[1]);
+        G[edge[1]].push_back(edge[0]);    
+    }
+    vector<vector<int>> result;
+    vector<bool> visited(V, false);
+
+    for(int u = 0; u < V; u++){
+        if(!visited[u]){
+            vector<int> temp;
+            dfs(G, u, visited, temp);
+            result.push_back(temp);
+        }
+    }
+    return result;
+}
+int main(){
+    return 0;
+}
