03b81ca34a76d79061f47c877c8f6a3201774d9d
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-11-22 11:38:04 +0530

    A couple 1000 rated

---
+++ CodeForces/1000_Rated/A_Red_Versus_Blue.cpp
@@ -0,0 +1,69 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- Typedefs ----------
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define uset unordered_set
+#define umap unordered_map
+
+
+// ---------- Macros ----------
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define vmax(a) (*max_element(all(a)))
+#define vmin(a) (*min_element(all(a)))
+#define vsum(a) (accumulate(all(a), 0LL))
+#define vsumd(a) (accumulate(all(a), 0.0))
+
+// ---------- Fast IO ----------
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+// ---------- Loops ----------
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define repd(i,a,b) for (int i = (a); i >= (b); i--)
+
+// ---------- Constants ----------
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+
+int main(){
+    fastio;
+    int T;
+    cin >> T;
+    while(T--){
+        ll n, r, b;
+        cin >> n >> r >> b;
+        string ans = "";
+        ll b_width = r/(b + 1);
+        ll rem = r%(b + 1);
+
+        for(int i = 0; i < b + 1; i++){
+            for(int j = 0; j < b_width; j++){
+                ans += "R";
+            }
+            if(rem > 0){
+                ans += "R";
+                rem--;
+            }
+            if(i != b)
+                ans += "B";
+        }
+        while(rem--)
+            ans += "R";
+        
+        cout << ans << endl;
+
+    }
+    return 0;
+}
