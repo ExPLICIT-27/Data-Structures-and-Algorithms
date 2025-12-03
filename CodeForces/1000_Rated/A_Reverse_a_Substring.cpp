fc80dede8c06560d1c4655ff99e21aab366782a8
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-12-03 16:01:27 +0530

    1000 rated completed

---
+++ CodeForces/1000_Rated/A_Reverse_a_Substring.cpp
@@ -0,0 +1,64 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- Typedefs ----------
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define uset unordered_set
+#define umap unordered_map
+
+
+// ---------- Macros ----------
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define vmax(a) (*max_element(all(a)))
+#define vmin(a) (*min_element(all(a)))
+#define vsum(a) (accumulate(all(a), 0LL))
+#define vsumd(a) (accumulate(all(a), 0.0))
+
+// ---------- Fast IO ----------a
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+// ---------- Loops ----------
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define repd(i,a,b) for (int i = (a); i >= (b); i--)
+
+// ---------- Constants ----------
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+
+int main(){
+    fastio;
+    
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+
+    int l = -1, r = -1;
+    rep(i, 1, n){
+        if(s[i] < s[i - 1]){
+            l = i, r = i + 1;
+            break;
+        }
+    }
+    if(l == -1){
+        cout << "NO\n";
+    }
+    else{
+        cout << "YES\n" << l << " " << r << endl;
+    }
+
+    return 0;
+}
