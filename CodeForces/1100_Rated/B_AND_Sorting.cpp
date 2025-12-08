171ca137171111554e0c5e90769275a44d5f108c
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-12-08 11:03:31 +0530

    daily CF quota

---
+++ CodeForces/1100_Rated/B_AND_Sorting.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- Typedefs ----------
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define uset unordered_set
+#define umap unordered_map
+
+// ---------- Macros ----------
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define vmax(a) (*max_element(all(a)))
+#define vmin(a) (*min_element(all(a)))
+#define vsum(a) (accumulate(all(a), 0LL))
+#define vsumd(a) (accumulate(all(a), 0.0))
+
+// ---------- Fast IO ----------
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+// ---------- Loops ----------
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define repd(i,a,b) for (int i = (a); i >= (b); i--)
+
+// ---------- Constants ----------
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+
+// ---------- PBDS ---------- policy based data strcutures, faster sets and maps
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+// Ordered Set (no duplicates)
+template<typename T>
+using ordered_set = tree<
+    T,
+    null_type,
+    less<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+// Ordered Multiset (allows duplicates using unique IDs)
+template<typename T>
+using ordered_multiset = tree<
+    pair<T,int>,
+    null_type,
+    less<pair<T,int>>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+// Ordered Map (key → value)
+template<typename K, typename V>
+using ordered_map = tree<
+    pair<K,V>,
+    null_type,
+    less<pair<K,V>>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+
+int main(){
+    fastio;
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+
+        vi A(n), B;
+
+        rep(i, 0, n){
+            cin >> A[i];
+            if(A[i] != i)
+                B.pb(A[i]);
+        }
+        ll ans = B[0];
+
+        rep(i, 1, sz(B))
+            ans = ans & B[i];
+
+        cout << ans << endl;
+
+    }
+
+    return 0;
+}
