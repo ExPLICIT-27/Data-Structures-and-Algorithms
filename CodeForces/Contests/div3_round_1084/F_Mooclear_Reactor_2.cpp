561659ff4da3b8867bf4e5565847cf577f60aae0
Nandu Mahesh <nandumahesh2004@gmail.com>
2026-03-02 22:14:27 +0530

    div 3 update

--- CodeForces/Contests/div3_round_1084/F_Mooclear_Reactor_2.cpp
+++ CodeForces/Contests/div3_round_1084/F_Mooclear_Reactor_2.cpp
@@ -86,24 +86,60 @@ ll binexp(ll a, ll b, ll M){
 void solve(){
     
     int n, m; cin >> n >> m;
-
-
-    vector<pll> bes(n), shp(m);
-
-    for(auto &p : bes)
-        cin >> p.ff >> p.ss;
-
-    for(auto &p : shp)
-        cin >> p.ff >> p.ss;
     
-    map<int, vll> particles;
+    /*
+    group based on y (reactivity)
+    in order to find the max value obtainable for any reactivity y, just find the sum of the highest y + 1 elements whose reactivity is >= y
+    in order to do this traverse from n to 1, keep track of max value possible for each type of reactivity
+    update max possible with the current values make sure the set always contains no more than i elements
+    in the same time, to answer the query keep track for each i, max sum possible if you exclude one element (obv the smallest) so that you can add the element in shop with this value to get total reactivity
+    */
 
-    for(auto &p : bes)
-        particles[p.ss].pb(p.ff);
+    vector<vector<ll>> A(n + 1);
 
-    for(auto &[x, y] : particles)
-        sort(all(y), greater<ll>());
-    map<int, ll> maxsum;
+
+    for(int i = 0; i < n; i++){
+        ll x, y;
+        cin >> x >> y;
+        A[y].push_back(x);
+    }
+
+    vector<ll> max_m1(n + 1, -INF); // max_m1[i] (max_m1 stands for max minus 1) = max value i - 1 elements with reactivity >= i - 1 (to be used in the queries )
+
+    ll maxposs = 0, curr = 0;
+    multiset<ll> ms;
+
+    for(int y = n; y >= 0; y--){
+        for(ll x : A[y]){
+            ms.insert(x);
+            curr += x;
+        }
+
+        // no of elements cannot exceed current reactivity values, reactivity y means can inlcude together y + 1 values
+        while(sz(ms) > (y + 1)){
+            curr -= *ms.begin();
+            ms.erase(ms.find(*ms.begin()));
+        }
+
+        if(sz(ms) == y + 1){
+            max_m1[y] = curr - *ms.begin(); // one less than
+        }
+        else if(sz(ms) == y){// alr one less than
+            max_m1[y] = curr;
+        }
+        maxposs = max(maxposs, curr);
+    }
+
+    for(int i = 1; i <= n; i++)
+        max_m1[i] = max(max_m1[i], max_m1[i - 1]); // if using 2 less elements (lesser reactivity) gives more output, then let that be the max for the given reactivity y
+    
+    for(int i = 0; i < m; i++){
+        ll x, y; cin >> x >> y;
+
+        cout << max(maxposs, x + max_m1[y]) << ' ';
+    }
+
+    cout << nline;
 
 
 }
